5aca1ca5907e0bcfdc712392f0c70f065fc79d13
Steven Harden <s.harden601@gmail.com>
2015-08-07 00:29:59 -0400

    Added favicon and changed email text format

--- js/emailer/cloud/main.js
+++ js/emailer/cloud/main.js
@@ -2,28 +2,30 @@
 // For example:
 Parse.Cloud.define("sendEmail", function(request, response) {
   var sendgrid = require("sendgrid");
-  sendgrid.initialize("your_sendgrid_username", "your_sendgrid_password");
+  Parse.Config.get().then(function(config){
+    sendgrid.initialize(config.get("sendgrid_username"), config.get("sendgrid_password"));
  
-  var fname = request.params.fname;
-  var lname = request.params.lname;
-  var email = request.params.email;
-  var company = request.params.company;
-  var message = request.params.message;
+    var fname = request.params.fname;
+    var lname = request.params.lname;
+    var email = request.params.email;
+    var company = request.params.company;
+    var message = request.params.message;
  
-  sendgrid.sendEmail({
-   to: "hello@thedreamcanvas.co",
-   from: email,
-   fromname: fname+" "+lname,
-   subject: "Message from DreamCanvas Website",
-   text: "Name: "+fname+" "+lname+"\nCompany: "+company+"\nEmail: "+email+"\nMessage:\n\n"+message
-   }, {
-     success: function(httpResponse) {
-       console.log(httpResponse);
-       response.success("Thanks for contacting DreamCanvas Solutions!");
-    },
-     error: function(httpResponse) {
-       console.error(httpResponse);
-       response.error("Uh oh, something went wrong");
-    }
+    sendgrid.sendEmail({
+     to: "hello@thedreamcanvas.co",
+     from: email,
+     fromname: fname+" "+lname,
+     subject: "Message from DreamCanvas Website",
+     text: "Name: "+fname+" "+lname+"\nCompany: "+company+"\nEmail: "+email+"\n\nMessage:\n"+message
+     }, {
+       success: function(httpResponse) {
+         console.log(httpResponse);
+         response.success("Thanks for contacting DreamCanvas Solutions!");
+      },
+       error: function(httpResponse) {
+         console.error(httpResponse);
+         response.error("Uh oh, something went wrong");
+      }
+    });
   });
 });
